5c0be2db871f7f9c0b26fc72fec5f4d2099aa112
leonardo-guglielmini <leonardo.guglielmini@outlook.it>
2024-10-23 16:51:22 +0200

    FIXED populateTable function

--- app.js
+++ app.js
@@ -6,8 +6,12 @@ const getInfo = document.getElementById("get-info");
 
 const userInfoTable =  document.getElementById("user-info-table");
 
+let userInfoValuesCounter=0;
 
-let userInfoValues = [];
+
+let userInfoValues = [
+    [],[],[],[],[]
+];
 
 const tableHeaders = ["Nominativo", "Lunghezza della tratta", "Età del passeggero", "Prezzo totale"];
 
@@ -16,7 +20,7 @@ const pricePerKm = 0.21
 
 function createTableHead(table,data){
     let thead =  table.createTHead();
-    let tbody = table.createTBody();
+    
     let trow = thead.insertRow();
     for(let i =0; i<data.length;i++){
         let th =  document.createElement("th");
@@ -24,15 +28,27 @@ function createTableHead(table,data){
         th.appendChild(text);
         trow.appendChild(th);
     }
-    return tbody;
 }
 
-function populateTable(tbody,data){
-    let trow = tableBody.insertRow();
-    for(let i=0; i<data.length;i++){
-        let td= trow.insertCell();
-        let text = document.createTextNode(data[i]);
-        td.appendChild(text);
+function populateTable(table,data,dataCounter){
+    
+    let tbody;
+    tbody = table.createTBody();
+     // reset tbody
+    if(dataCounter>0){
+        //console.log("tbody removed")
+        table.removeChild(table.getElementsByTagName("tBody")[0]);
+    }
+    
+
+    for(let i=0; i<dataCounter+1;i++){
+        let trow = tbody.insertRow();
+        for(let j=0;j<data[i].length;j++){
+            let td= trow.insertCell();
+            let text = document.createTextNode(data[i][j]);
+            td.appendChild(text);
+        }
+        
     }
 }
 
@@ -40,12 +56,11 @@ function getFinalPrice(length, age, price){
 
     let basePrice = length*price;
 
-    return(age<18 ? ((basePrice/100)*80) : age>65 ? ((basePrice/100)*60) : basePrice)
+    return(age<18 ? ((basePrice/100)*80) : age>65 ? ((basePrice/100)*60) : basePrice).toFixed(2)
 
 }
 
-
-let tableBody=createTableHead(userInfoTable, tableHeaders);
+createTableHead(userInfoTable, tableHeaders);
 
 getInfo.addEventListener("submit", function() {
     event.preventDefault();
@@ -53,10 +68,20 @@ getInfo.addEventListener("submit", function() {
     let tripLengthValue = tripLength.value;
     let userAgeValue = userAge.value;
     let finalPrice=getFinalPrice(tripLengthValue, userAgeValue, pricePerKm);
-    userInfoValues.push(userNameValue,tripLengthValue,userAgeValue,finalPrice);
-    //console.log(`Il costo finale del biglietto risulta: ${finalPrice.toFixed(2)} \u20ac`);
 
-    populateTable(tableBody, userInfoValues);
+    let tempArray=[];
+    tempArray.push(userNameValue,tripLengthValue,userAgeValue,finalPrice);
+
+    for(let i=0;i<4;i++){
+        userInfoValues[userInfoValuesCounter][i]=tempArray[i];
+    }       
+
+    //console.log(`Il costo finale del biglietto risulta: ${finalPrice.toFixed(2)} \u20ac`);
+    //console.log(userInfoValues[userInfoValuesCounter])
+    
+    populateTable(userInfoTable, userInfoValues, userInfoValuesCounter);
+
+    userInfoValuesCounter++;
 
     userName.value="";
     tripLength.value="";
