99bbad948154b50a823ccbe66ddb6005def47953
leonardo-guglielmini <leonardo.guglielmini@outlook.it>
2024-10-23 14:05:31 +0200

    ADD html | input value length/age, button ADD js | function getFinalPrice, button event listener

---
+++ app.js
@@ -0,0 +1,26 @@
+const tripLength = document.getElementById("trip-length");
+
+const userAge = document.getElementById("user-age");
+
+const getInfo = document.getElementById("get-info");
+
+
+
+const pricePerKm = 0.21
+
+function getFinalPrice(length, age, price){
+
+    let basePrice = length*price;
+
+    return(age<18 ? ((basePrice/100)*80) : age>65 ? ((basePrice/100)*60) : basePrice)
+
+}
+
+
+getInfo.addEventListener("click", function() {
+    let tripLengthValue= tripLength.value;
+    let userAgeValue = userAge.value;
+    let finalPrice=getFinalPrice(tripLengthValue, userAgeValue, pricePerKm);
+    console.log(`Il costo finale del biglietto risulta: ${finalPrice.toFixed(2)} \u20ac`);
+})
+
