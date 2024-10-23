76a18116cb1af49e66be2c87e8c9ccb9a0b76214
leonardo-guglielmini <leonardo.guglielmini@outlook.it>
2024-10-23 15:38:38 +0200

    ADD html | table user info ADD js | generateTable, populateTable functions TO-DO fix populateTable

--- app.js
+++ app.js
@@ -1,12 +1,40 @@
 const tripLength = document.getElementById("trip-length");
-
 const userAge = document.getElementById("user-age");
+const userName = document.getElementById("user-name");
 
 const getInfo = document.getElementById("get-info");
 
+const userInfoTable =  document.getElementById("user-info-table");
+
+
+let userInfoValues = [];
+
+const tableHeaders = ["Nominativo", "Lunghezza della tratta", "Età del passeggero", "Prezzo totale"];
 
 
 const pricePerKm = 0.21
+
+function createTableHead(table,data){
+    let thead =  table.createTHead();
+    let tbody = table.createTBody();
+    let trow = thead.insertRow();
+    for(let i =0; i<data.length;i++){
+        let th =  document.createElement("th");
+        let text = document.createTextNode(data[i]);
+        th.appendChild(text);
+        trow.appendChild(th);
+    }
+    return tbody;
+}
+
+function populateTable(tbody,data){
+    let trow = tableBody.insertRow();
+    for(let i=0; i<data.length;i++){
+        let td= trow.insertCell();
+        let text = document.createTextNode(data[i]);
+        td.appendChild(text);
+    }
+}
 
 function getFinalPrice(length, age, price){
 
@@ -17,11 +45,21 @@ function getFinalPrice(length, age, price){
 }
 
 
+let tableBody=createTableHead(userInfoTable, tableHeaders);
+
 getInfo.addEventListener("submit", function() {
     event.preventDefault();
-    let tripLengthValue= tripLength.value;
+    let userNameValue = userName.value;
+    let tripLengthValue = tripLength.value;
     let userAgeValue = userAge.value;
     let finalPrice=getFinalPrice(tripLengthValue, userAgeValue, pricePerKm);
-    console.log(`Il costo finale del biglietto risulta: ${finalPrice.toFixed(2)} \u20ac`);
+    userInfoValues.push(userNameValue,tripLengthValue,userAgeValue,finalPrice);
+    //console.log(`Il costo finale del biglietto risulta: ${finalPrice.toFixed(2)} \u20ac`);
+
+    populateTable(tableBody, userInfoValues);
+
+    userName.value="";
+    tripLength.value="";
+    userAge.value="";
 })
 
